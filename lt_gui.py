33887326eebe602b06fe5c7a9ada3910f149b669
Garrett F <38448592+gallimania@users.noreply.github.com>
2018-07-10 10:40:03 -0500

    Create lt_gui.py

---
+++ lt_gui.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2018 Garrett Fifer
+#
+# This file provided the functions that supply the main program with an easy to
+# use graphical user interface. Uses the tkinter package supplied with most python
+# installations by default.
+#
+# begin lt_gui.py
+# 
+
+from tkinter import Tk, Label, Button, OptionMenu, Entry
+
+lt_gui = Tk()
