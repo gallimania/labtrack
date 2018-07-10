92dae73fe5eddf8294f31a7e5034337e955cc6e8
Garrett F <38448592+gallimania@users.noreply.github.com>
2018-07-10 10:08:13 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2018 Garrett Fifer
+#
+# This software is licensed under the GNU GPL V3 software license.
+# Please become familiar with such licensing before making use of this software.
+#
+# While it is certainly not required, if you find this software or code helpful 
+# I am able and happy to accept donations in Bitcoin, Ethereum, and Litecoin (adresses below).
+#
+# BTC Adress:
+# ETH Adress:
+# LTC Adress:
+#
+# I do this all in my spare time so any amount of money helps me to continue the development
+# of this project.
+#
+# main.py
+
+import lt_gui
+import f
+
