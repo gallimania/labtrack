f39c8bd03d522e2f6b7d9e3ca9d6e33ac84a1536
Garrett F <38448592+gallimania@users.noreply.github.com>
2018-07-10 11:01:00 -0500

    Update lt_gui.py

--- lt_gui.py
+++ lt_gui.py
@@ -1,11 +1,10 @@
 # Copyright (C) 2018 Garrett Fifer
 #
-# This file provided the functions that supply the main program with an easy to
-# use graphical user interface. Uses the tkinter package supplied with most python
-# installations by default.
+# This file provides the functions that supply the main.py program with an easy to
+# use graphical user interface. Makes use of the tkinter module supplied with most python
+# installations.
 #
 # begin lt_gui.py
-# 
 
 from tkinter import Tk, Label, Button, OptionMenu, Entry
 
