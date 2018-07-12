08df30f602ac8cfb9687f94c2dcafdd91fd1684d
Garrett F <38448592+gallimania@users.noreply.github.com>
2018-07-12 12:30:56 -0500

    Update sql_com.py

--- sql_com.py
+++ sql_com.py
@@ -7,14 +7,15 @@ sql_conn = sql3.connect('inventory.db')
 sql_curs = sql_conn.cursor()
 
 def create_table(entry_tableName):
-  ''' Creates a table within the created database file '''
+  ''' Creates a table within the created database file using the table name specified by the
+  variable entry_tableName '''
   sql_curs.execute('''
-  CREATE TABLE entry_tableName(chemical_name TEXT, cas_num TEXT, purchase_price TEXT, purchase_amt TEXT
-  ''')
+  CREATE TABLE {} (chemical_name TEXT, cas_num TEXT, purchase_price TEXT, purchase_amt TEXT) INSERT (?,?,?,?)
+  ''').format(entry_tableName)
 
-def new_entry():
+def new_entry(chemical_name, cas_num, purchase_price, purchase_amt):
   ''' Function that inserts a new row of data into the specified table. '''
   sql_curs.execute('''
-  INSERT INTO chemicals VALUES('NULL','NULL','NULL','NULL')
-  ''')
+  INSERT INTO chemicals VALUES ({},{},{},{})
+  ''').format(chemical_name, cas_num, purchase_price, purchase_amt)
   
